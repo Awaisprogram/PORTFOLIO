f544b1a834bde3a9bfb59ea57346e5c866909df5
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2024-08-27 17:28:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-icon").onclick = function() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+document.getElementById("closebtn").onclick = function() {
+    document.getElementById("sidebar").style.width = "0";
+}
